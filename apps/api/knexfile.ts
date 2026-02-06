0a92bab85d231209ea3a3f14356cf47e0df64191
Jarif Ahmed Soumik <jasoumik@gmail.com>
2026-02-07 04:21:51 +0600

    fix: fix env dir issue

--- apps/api/knexfile.ts
+++ apps/api/knexfile.ts
@@ -1,7 +1,11 @@
 import type { Knex } from 'knex';
 import * as dotenv from 'dotenv';
+import path from 'path';
 
-dotenv.config();
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+});
+
 
 const config: Knex.Config = {
   client: 'postgresql',
