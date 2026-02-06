79307e50c94d5efd5ac47c0b7380d115b3bc8780
Jarif Ahmed Soumik <jasoumik@gmail.com>
2026-02-07 04:36:41 +0600

    fix: dotenv

--- apps/api/knexfile.ts
+++ apps/api/knexfile.ts
@@ -3,7 +3,11 @@ import * as dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({
-  path: path.resolve(__dirname, '../../.env'),
+  path: [
+    path.resolve(__dirname, '../../.env.local'),
+    path.resolve(__dirname, '../../.env.production'),
+    path.resolve(__dirname, '../../.env'),
+  ],
 });
 
 
